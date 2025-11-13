22b5ecabbae1664be4db27aa5e850e6b45de1acf
eosxh <emirshn000@gmail.com>
2025-11-13 16:05:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 The somebody say : just eat 
 
 wget -O- https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip | bsdtar -xvf- -C ~/.local/share/fonts && fc-cache -fv
+
+
+
+
+
+
+
+
+mkdir -p ~/.local/share/fonts && cd ~/.local/share/fonts && wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip && unzip -o JetBrainsMono.zip && rm JetBrainsMono.zip && fc-cache -fv
